a5c753e1a372d7c6ceb0960df19ac6167c268fe4
sofinico <nicolettisofia1@gmail.com>
2024-05-15 21:27:55 +0200

    level: NaughtCoin

--- test/NaughtCoin.t.sol
+++ test/NaughtCoin.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {Utils} from "./utils/Utils.sol";
 
 import {NaughtCoin} from "../src/levels/NaughtCoin.sol";
@@ -11,26 +11,57 @@ import {NaughtCoinAttack} from "../src/attacks/NaughtCoinAttack.sol";
 import {Level} from "src/levels/base/Level.sol";
 import {Ethernaut} from "src/Ethernaut.sol";
 
-address constant DEPLOYER = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
+contract TestNaughtCoin is Test, Utils {
+    Ethernaut ethernaut;
+    NaughtCoin instance;
 
-contract NaughtCoinAttackTest is Test {
-    NaughtCoin naughtCoin;
-    NaughtCoinAttack naughtCoinAttack;
+    address payable owner;
+    address payable player;
 
+    /// @notice Create level instance.
     function setUp() public {
-        naughtCoin = new NaughtCoin(DEPLOYER);
-        naughtCoinAttack = new NaughtCoinAttack{value: 0.001 ether}(address(naughtCoin));
+        address payable[] memory users = createUsers(2);
+
+        owner = users[0];
+        vm.label(owner, "Owner");
+
+        player = users[1];
+        vm.label(player, "Player");
+
+        vm.startPrank(owner);
+        ethernaut = getEthernautWithStatsProxy(owner);
+        NaughtCoinFactory factory = new NaughtCoinFactory();
+        ethernaut.registerLevel(Level(address(factory)));
+        vm.stopPrank();
+
+        vm.startPrank(player);
+        instance = NaughtCoin(
+            payable(createLevelInstance(ethernaut, Level(address(factory)), 0))
+        );
+        vm.stopPrank();
     }
 
-    function testAttack() public {
-        uint balance = naughtCoin.balanceOf(DEPLOYER);
-        vm.prank(DEPLOYER);
-        naughtCoin.approve(address(naughtCoinAttack), balance);
-        
-        vm.prank(DEPLOYER);
-        naughtCoinAttack.transferTokens();
-        
-        uint newBalance = naughtCoin.balanceOf(DEPLOYER);
+    /// @notice Check the intial state of the level and enviroment.
+    function testInit() public {
+        vm.startPrank(player);
+        assertFalse(submitLevelInstance(ethernaut, address(instance)));
+    }
+
+    /// @notice Test the solution for the level.
+    function testSolve() public {
+        vm.startPrank(player);
+        NaughtCoinAttack attack = new NaughtCoinAttack{value: 0.001 ether}(
+            address(instance)
+        );
+
+        uint balance = instance.balanceOf(player);
+        instance.approve(address(attack), balance);
+
+        attack.transferTokens();
+
+        uint newBalance = instance.balanceOf(player);
         assertEq(newBalance, 0);
+
+        assertTrue(submitLevelInstance(ethernaut, address(instance)));
     }
 }
