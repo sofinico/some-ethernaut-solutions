12f83bf79e29366a661c960a202205600e9211ba
sofinico <nicolettisofia1@gmail.com>
2024-05-18 03:04:34 +0200

    chore: indent in ts files

--- test/GatekeeperOne.ts
+++ test/GatekeeperOne.ts
@@ -1,72 +1,88 @@
-import { TransactionDescription, TransactionReceipt, TransactionResponse, ethers } from "ethers";
+import {
+  TransactionDescription,
+  TransactionReceipt,
+  TransactionResponse,
+  ethers,
+} from "ethers";
 import solutionABI from "./utils/GatekeeperOneSolutionABI-a553138ec8d0619eaf4c65eec457bceb08aa8460.json";
 
 const provider = new ethers.JsonRpcProvider("https://rpc.sepolia.org");
 
 // fetch a transaction
-async function getTransaction(txHash: string): Promise<TransactionResponse | null>  {
-    try {
-        const transaction = await provider.getTransaction(txHash);
-        return transaction;
-    } catch (error) {
-        console.error(`Failed to fetch transaction: ${error}`);
-        return null;
-    }
+async function getTransaction(
+  txHash: string
+): Promise<TransactionResponse | null> {
+  try {
+    const transaction = await provider.getTransaction(txHash);
+    return transaction;
+  } catch (error) {
+    console.error(`Failed to fetch transaction: ${error}`);
+    return null;
+  }
 }
 
 // fetch a receipt
-async function getTransactionReceipt(txHash: string): Promise<TransactionReceipt | null>  {
-    try {
-        const transaction = await provider.getTransactionReceipt(txHash);
-        return transaction;
-    } catch (error) {
-        console.error(`Failed to fetch transaction: ${error}`);
-        return null;
-    }
+async function getTransactionReceipt(
+  txHash: string
+): Promise<TransactionReceipt | null> {
+  try {
+    const transaction = await provider.getTransactionReceipt(txHash);
+    return transaction;
+  } catch (error) {
+    console.error(`Failed to fetch transaction: ${error}`);
+    return null;
+  }
 }
 
 /**
- * me interesa saber cuánto gas se consume hasta el gasleft() 
+ * me interesa saber cuánto gas se consume hasta el gasleft()
  * y por qué ese valor varía en las diferentes blockchains.
  */
 
 async function getConsumedGas() {
-    /**
-     * transaction hash of me calling makeEntrant() on first instance (no event)
-     * contract solution version as in commit 659915429e7a1d7ec5d84d74633c606956c5ffc5
-     */
+  /**
+   * transaction hash of me calling makeEntrant() on first instance (no event)
+   * contract solution version as in commit 659915429e7a1d7ec5d84d74633c606956c5ffc5
+   */
 
-    // const txHash = "0xa511f48e8435ae21414f491fdf36ea73efc387a04fcb93706c0cb9c7b014660c";
-    
-    /**
-     * transaction hash of me calling makeEntrant() on second instance (event emitted)
-     * solution on Sepolia was with version of the contract as in commit a553138ec8d0619eaf4c65eec457bceb08aa8460 
-     * with ForwardedGas() event instead of GasOffset() as it is now.
-     */
+  // const txHash = "0xa511f48e8435ae21414f491fdf36ea73efc387a04fcb93706c0cb9c7b014660c";
 
-    const txHash = "0x4c50f2e5aa9a8afbfe7dd128ccb80543043ee6bdbd0ab98a59593bd1447b3690";
+  /**
+   * transaction hash of me calling makeEntrant() on second instance (event emitted)
+   * solution on Sepolia was with version of the contract as in commit a553138ec8d0619eaf4c65eec457bceb08aa8460
+   * with ForwardedGas() event instead of GasOffset() as it is now.
+   */
 
-    const ISolution = new ethers.Interface(solutionABI);
-    
-    const transactionResponse = await getTransaction(txHash) as TransactionResponse;
-    // console.log(transactionResponse);
-    const txData = transactionResponse.data;
-    // console.log(txData);
-    const decodedData = ISolution.parseTransaction({ data: txData }) as TransactionDescription;
-    
-    // console.log(`Function name: ${decodedData.name}`);
-    // console.log(`Function signature: ${decodedData.signature}`);
-    // console.log(`Decoded arguments: `, decodedData.args);
-    
-    const { logs } = await getTransactionReceipt(txHash) as TransactionReceipt;
-    const forwardedGas = BigInt(logs[0].data);
-    const multiplier = decodedData.args[1];
-    const consumedGas = forwardedGas - BigInt(8191)*multiplier;
-    
-    console.log(`Total forwarded gas to GatekeeperOne.entrant(): ${forwardedGas.toString()}`);
-    console.log(`Total consumed gas until gasleft(): ${consumedGas.toString()}`);
-};
+  const txHash =
+    "0x4c50f2e5aa9a8afbfe7dd128ccb80543043ee6bdbd0ab98a59593bd1447b3690";
+
+  const ISolution = new ethers.Interface(solutionABI);
+
+  const transactionResponse = (await getTransaction(
+    txHash
+  )) as TransactionResponse;
+  // console.log(transactionResponse);
+  const txData = transactionResponse.data;
+  // console.log(txData);
+  const decodedData = ISolution.parseTransaction({
+    data: txData,
+  }) as TransactionDescription;
+
+  // console.log(`Function name: ${decodedData.name}`);
+  // console.log(`Function signature: ${decodedData.signature}`);
+  // console.log(`Decoded arguments: `, decodedData.args);
+
+  const { logs } = (await getTransactionReceipt(txHash)) as TransactionReceipt;
+  const forwardedGas = BigInt(logs[0].data);
+  const multiplier = decodedData.args[1];
+  const consumedGas = forwardedGas - BigInt(8191) * multiplier;
+
+  console.log(
+    `Total forwarded gas to GatekeeperOne.entrant(): ${forwardedGas.toString()}`
+  );
+  console.log(`Total consumed gas until gasleft(): ${consumedGas.toString()}`);
+}
 
 getConsumedGas().catch((error) => {
-    console.error("Error getConsumedGas():", error);
+  console.error("Error getConsumedGas():", error);
 });
--- test/Preservation.ts
+++ test/Preservation.ts
@@ -7,37 +7,40 @@ const abiCode = new ethers.AbiCoder();
 const levelInstanceAddress = "0xE672F969e0b09942f87436c956Abe0B90BfD5eFA";
 
 async function getBytecode() {
-    // get bytecode
-    const bytecode = await provider.getCode(levelInstanceAddress);
-    console.log("Preservation bytecode:", bytecode);
+  // get bytecode
+  const bytecode = await provider.getCode(levelInstanceAddress);
+  console.log("Preservation bytecode:", bytecode);
 }
 
-async function getPreservationData() {    
-    let encodedData = await provider.getStorage(levelInstanceAddress, 0);
-    const timeZone1Library = abiCode.decode(["address"], encodedData);
-    console.log("Storage [0]", encodedData);
-    console.log("timeZone1Library", timeZone1Library);
+async function getPreservationData() {
+  let encodedData = await provider.getStorage(levelInstanceAddress, 0);
+  const timeZone1Library = abiCode.decode(["address"], encodedData);
+  console.log("Storage [0]", encodedData);
+  console.log("timeZone1Library", timeZone1Library);
 
-    encodedData = await provider.getStorage(levelInstanceAddress, 1);
-    const timeZone2Library = abiCode.decode(["address"], encodedData);
-    console.log("Storage [1]", encodedData);
-    console.log("timeZone2Library", timeZone2Library);
+  encodedData = await provider.getStorage(levelInstanceAddress, 1);
+  const timeZone2Library = abiCode.decode(["address"], encodedData);
+  console.log("Storage [1]", encodedData);
+  console.log("timeZone2Library", timeZone2Library);
 
-    encodedData = await provider.getStorage(levelInstanceAddress, 2);
-    const owner = abiCode.decode(["address"], encodedData);
-    console.log("Storage [2]", encodedData);
-    console.log("owner", owner);
+  encodedData = await provider.getStorage(levelInstanceAddress, 2);
+  const owner = abiCode.decode(["address"], encodedData);
+  console.log("Storage [2]", encodedData);
+  console.log("owner", owner);
 
-    encodedData = await provider.getStorage(levelInstanceAddress, 3);
-    const storedTime = abiCode.decode(["uint256"], encodedData)
-    console.log("Storage [3]", encodedData);
-    console.log("storedTime", storedTime);
-    const date = new Date(String(storedTime[0] * BigInt(1000)));
-    console.log("storedTime as date", date);
+  encodedData = await provider.getStorage(levelInstanceAddress, 3);
+  const storedTime = abiCode.decode(["uint256"], encodedData);
+  console.log("Storage [3]", encodedData);
+  console.log("storedTime", storedTime);
+  const date = new Date(String(storedTime[0] * BigInt(1000)));
+  console.log("storedTime as date", date);
 
-    console.log("Casted address:", ethers.toBigInt("0xA7a47ce31946D2d67196f0C95EC6314Df12FbCfa").toString());
+  console.log(
+    "Casted address:",
+    ethers.toBigInt("0xA7a47ce31946D2d67196f0C95EC6314Df12FbCfa").toString()
+  );
 }
 
 getPreservationData().catch((error) => {
-    console.error("Error getPreservationData():", error);
-}); 
+  console.error("Error getPreservationData():", error);
+});
